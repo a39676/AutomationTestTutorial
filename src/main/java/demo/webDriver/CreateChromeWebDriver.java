2aaf067a5ec0832bdede3352396e097c4a3e4ff3
DavenChan <q39676@gmail.com>
2021-09-29 12:27:45 +0800

    Update
    Spring Boot + Selenium web driver

--- src/main/java/demo/webDriver/CreateChromeWebDriver.java
+++ src/main/java/demo/webDriver/CreateChromeWebDriver.java
@@ -5,28 +5,18 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Component;
 
-public class CreateChromeDriver {
-
-	public static void main(String[] args) {
-		WebDriver driver = buildChromeWebDriver();
-		driver.get("https://zhang3.xyz");
-		try {
-			// 等待10秒
-			Thread.sleep(10000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		driver.quit(); // 退出浏览器
-	}
-
-	public static WebDriver buildChromeWebDriver() {
-		// chrome web driver path
+@Component
+public class CreateChromeWebDriver {
+	
+	public WebDriver buildFireFoxWebDriver() {
+		// chromedriver path 
 		String path = "D:/auxiliary/seleniumWebDriver/chromedriver.exe";
 		String driverType = "webdriver.chrome.driver";
 		System.setProperty(driverType, path);
 		ChromeOptions options = new ChromeOptions();
-
+		
 		// options.addArguments("--headless"); // 如有需要(Linux模拟环境下), 指定"无头模式", 则不出现浏览器窗口
 
 		ChromeDriver driver = new ChromeDriver(options);
@@ -34,5 +24,5 @@ public class CreateChromeDriver {
 		driver.manage().window().maximize(); // 窗口自动最大化
 		return driver;
 	}
-
+	
 }
--- src/main/java/demo/webDriver/CreateFireFoxWebDriver.java
+++ src/main/java/demo/webDriver/CreateFireFoxWebDriver.java
@@ -5,28 +5,18 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.springframework.stereotype.Component;
 
-public class CreateFireFoxDriver {
-
-	public static void main(String[] args) {
-		WebDriver driver = buildFireFoxWebDriver();
-		driver.get("https://zhang3.xyz");
-		try {
-			// 等待10秒
-			Thread.sleep(10000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		driver.quit(); // 退出浏览器
-	}
-
-	public static WebDriver buildFireFoxWebDriver() {
-		// geckodriver path
+@Component
+public class CreateFireFoxWebDriver {
+	
+	public WebDriver buildFireFoxWebDriver() {
+		// geckodriver path 
 		String path = "D:/auxiliary/seleniumWebDriver/geckodriver.exe";
 		String driverType = "webdriver.gecko.driver";
 		System.setProperty(driverType, path);
 		FirefoxOptions options = new FirefoxOptions();
-
+		
 		// options.addArguments("--headless"); // 如有需要(Linux模拟环境下), 指定"无头模式", 则不出现浏览器窗口
 
 		FirefoxDriver driver = new FirefoxDriver(options);
@@ -34,5 +24,5 @@ public class CreateFireFoxDriver {
 		driver.manage().window().maximize(); // 窗口自动最大化
 		return driver;
 	}
-
+	
 }
