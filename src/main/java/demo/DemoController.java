c48de3b52a589590fd95c73e134b84eeadce3402
DavenChan <q39676@gmail.com>
2021-09-29 14:29:51 +0800

    Update
    Spring boot with MyBatis

--- src/main/java/demo/DemoController.java
+++ src/main/java/demo/DemoController.java
@@ -1,5 +1,8 @@
 package demo;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import demo.mapper.StudentMapper;
+import demo.pojo.po.Student;
+import demo.pojo.po.StudentExample;
 import demo.webDriver.CreateChromeWebDriver;
 import demo.webDriver.CreateFireFoxWebDriver;
 
@@ -21,6 +27,18 @@ public class DemoController {
 	private CreateFireFoxWebDriver fireFoxDriver;
 	@Autowired
 	private CreateChromeWebDriver chromeDriver;
+	@Autowired
+	private StudentMapper studentMapper;
+	
+	@GetMapping(value = "/queryStudent")
+	@ResponseBody
+	public List<String> queryStudent() {
+		StudentExample example = new StudentExample();
+		example.createCriteria().andIdBetween(2, 5);
+		List<Student> studentList = studentMapper.selectByExample(example);
+		List<String> studentNameList = studentList.stream().map(po -> po.getStudentName()).collect(Collectors.toList());
+		return studentNameList;
+	}
 
 	@GetMapping(value = "/createFireFox")
 	@ResponseBody
@@ -28,12 +46,6 @@ public class DemoController {
 		WebDriver driver = fireFoxDriver.buildFireFoxWebDriver();
 		driver.get("http://zhang3.xyz"); // 访问 zhang3.xyz
 
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
 		WebElement link = driver.findElement(By.xpath("//*[contains(text(), 'UI自动化_环境搭建')]")); // 查找"UI自动化_环境搭建" 的链接
 
 		link.click(); // 点击
@@ -55,12 +67,6 @@ public class DemoController {
 		WebDriver driver = chromeDriver.buildFireFoxWebDriver();
 		driver.get("http://zhang3.xyz"); // 访问 zhang3.xyz
 
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
 		WebElement link = driver.findElement(By.xpath("//*[contains(text(), 'UI自动化_环境搭建')]")); // 查找"UI自动化_环境搭建" 的链接
 
 		link.click(); // 点击
